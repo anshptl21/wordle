28969adeef92c5b48a9c57bf5946a988e44bc827
anshptl21 <85411358+anshptl21@users.noreply.github.com>
2022-03-08 16:56:03 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -17,8 +17,8 @@
     <h1> Urban Charms
     <hr id="solid">
  </h1>
-  <div class="nav"><a target="_blank" href="index.html">Home</a> <a target="_blank" href="product.html">Products</a>
-<a target="_blank" href="contact.html">Contact</a>
+  <div class="nav"><a target="_blank" href="index.php">Home</a> <a target="_blank" href="product.php">Products</a>
+<a target="_blank" href="contact.php">Contact</a>
 </div>
 <div id = "menu"><span class="hyperlinks"><a target="_blank" href="index.html">Home</a> <a target="_blank" href="notavaliable.html">Products</a>
 <a target="_blank" href="contact.html">Contact</a></span></div>
@@ -73,6 +73,12 @@
     <label for="text">Message</label>  
     <textarea type = "text" class="text" placeholder="Message" name= Message required rows="4" cols="50"></textarea>
     <div><button type=”submit” class="btn">Submit<button></div>
+     <?php
+if($_POST["Message"]) {
+mail("anshptl21@gmail.com", "Contact form",
+$_POST["Insert Your Message"]. "From: anshptl21@gmail.com");
+}
+?>
 </form>
 </div>
 </div>
@@ -86,7 +92,13 @@
     <label for="email" ><b></b></label>
     <input type="text" class="subscribe textarea" placeholder="example@gmail.com" name="email" required>
        <div> <button type="button" class="subscribe button">Submit</button></div
-></form>
+>
+      <?php
+if($_POST["Message"]) {
+mail("anshptl21@gmail.com", "Contact form",
+$_POST["Insert Your Message"]. "From: anshptl21@gmail.com");
+}
+?></form>
 </div>
 </div>
 </footer>
