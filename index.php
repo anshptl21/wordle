2d331d641c627911e27a0b3706a6e4009863c436
anshptl21 <85411358+anshptl21@users.noreply.github.com>
2022-03-08 16:57:19 -0800

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
  	<h1> Urban Charms
  	<hr id="solid">
  </h1>
- <div class="nav"><a target="_blank" href="index.html">Home</a> <a target="_blank" href="product.html">Products</a>
-<a target="_blank" href="contact.html">Contact</a>
+ <div class="nav"><a target="_blank" href="index.php">Home</a> <a target="_blank" href="product.php">Products</a>
+<a target="_blank" href="contact.php">Contact</a>
 </div>
-<div id = "menu"><span class="hyperlinks"><a target="_blank" href="home.html">Home</a> <a target="_blank" href="notavaliable.html">Products</a>
-<a target="_blank" href="contact.html">Contact</a></span></div>
+<div id = "menu"><span class="hyperlinks"><a target="_blank" href="home.php">Home</a> <a target="_blank" href="product.php">Products</a>
+<a target="_blank" href="contact.php">Contact</a></span></div>
 	<table >
 		<tbody>
 			<tr>
@@ -64,7 +64,12 @@
 
     <label for="email" ><b></b></label>
     <input type="text" class="subscribe textarea" placeholder="example@gmail.com" name="email" required>
-       <div> <button type="button" class="subscribe button">Submit</button></div>
+       <div> <button type="button" class="subscribe button">Submit</button></div> <?php
+if($_POST["Message"]) {
+mail("anshptl21@gmail.com", "Contact form",
+$_POST["Insert Your Message"]. "From: anshptl21@gmail.com");
+}
+?>
 </form>
 </div>
 </div>
